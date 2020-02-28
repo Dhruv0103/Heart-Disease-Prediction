144ce246710e00db3de27a15e38c1e25dd1e0ba7
Dhruv0103 <61445275+Dhruv0103@users.noreply.github.com>
2020-02-28 22:11:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import math
+import pandas as pd
+import h5py
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.python.framework import ops
+import pickle
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+global graph
+graph = tf.get_default_graph() 
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    dj = np.asarray(int_features)
+    dj = dj.reshape(1,13)
+    with graph.as_default():
+        y_hat = model.predict([dj])
+        y_hat=float(y_hat[0])
+
+    return render_template('index.html', prediction_text='Heart Disease chances are {} %'.format(y_hat*100))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
